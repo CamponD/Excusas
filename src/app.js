40f99aea6fd850d956e63848f08fd4cc2ce36320
Daniel Campón Perdigones <campondaniel@gmail.com>
2024-08-06 08:37:00 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+import "./index.html";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
+
+let quien = ["My dog ", "My grandma ", "The dentist ", "the neighbor's cat "];
+let accion = ["bit ", "found ", "overdid it with ", "almost drowned "];
+let objeto = [
+  "neighbor's son ",
+  "my hidden magazines ",
+  "with the anesthesia ",
+  "in my pool "
+];
+let cuando = [
+  "while playing",
+  "while cleaning my room",
+  "during my operation",
+  "while eating"
+];
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let random1 = Math.floor(Math.random() * 4);
+  let random2 = Math.floor(Math.random() * 4);
+  let random3 = Math.floor(Math.random() * 4);
+  let random4 = Math.floor(Math.random() * 4);
+
+  let textoCambiado = document.querySelector(".excusa");
+  textoCambiado.innerHTML =
+    quien[random1] + accion[random2] + objeto[random3] + cuando[random4];
 };
